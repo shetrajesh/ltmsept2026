c3b713a3ca9e177856c509b086f5cf4f807f39f6
kochi0903 <kroy8900391997@gmail.com>
2026-09-07 10:00:58 +0530

    Student Model Initialised

--- src/main/java/com/examly/ltmsept2026/model/Student.java
+++ src/main/java/com/examly/ltmsept2026/model/Student.java
@@ -1,5 +1,19 @@
 package com.examly.ltmsept2026.model;
 
 public class Student {
+    private int id;
+    private String name;
 
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
