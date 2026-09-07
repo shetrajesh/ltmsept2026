b95924d9559cbc75bad1fbfe1171779d08de9202
rshet007@yahoo.com <rshet007@yahoo.com>
2026-09-07 09:44:48 +0530

    First commit 07-09-202 09:44

---
+++ src/main/java/com/examly/ltmsept2026/Ltmsept2026Application.java
@@ -0,0 +1,13 @@
+package com.examly.ltmsept2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ltmsept2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ltmsept2026Application.class, args);
+	}
+
+}
